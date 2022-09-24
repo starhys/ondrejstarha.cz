470636a415e3dc90e431b65d83eaf881633151ec
STARhys <o.starha@gmail.com>
2022-09-24 22:11:24 +0200

    added google analytics + edit css

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,20 @@ module.exports = {
     title: config.siteTitle,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-W8W31Y5QS6', // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
